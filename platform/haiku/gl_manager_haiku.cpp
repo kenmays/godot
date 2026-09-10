44d6bf017615a453518e619261e65aa240169702
kenmays <maybird1776@yahoo.com>
2026-09-09 22:23:27 -0700

    Fix Haiku BGLView LockGL return handling

--- platform/haiku/gl_manager_haiku.cpp
+++ platform/haiku/gl_manager_haiku.cpp
@@ -2,8 +2,6 @@
 
 #include "haiku_gl_view.h"
 
-#include <GL/gl.h>
-
 status_t GLManagerHaiku::initialize()
 {
 	return B_OK;
@@ -13,7 +11,8 @@ status_t GLManagerHaiku::make_current(BGLView *view)
 {
 	if (view == nullptr)
 		return B_BAD_VALUE;
-	return view->LockGL() ? B_OK : B_ERROR;
+	view->LockGL();
+	return B_OK;
 }
 
 void GLManagerHaiku::release_current(BGLView *view)
